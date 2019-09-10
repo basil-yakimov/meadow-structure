f46b53c5ce30c89ac62c003e9c33396555a95812
arrirh <arrirh@gmail.com>
2019-09-10 20:24:43 +0300

    perfomed phylogenetic analysis

--- 01.make-phylo.r
+++ 01.make-phylo.r
@@ -4,7 +4,6 @@ library(dplyr)
 library(readxl)
 
 load("clean.data/meadows-2018.rda")
-rm(meta)
 
 gf <- tips.info[, c(3,4)] %>% group_by(family, genus) %>% slice(1) %>% ungroup()
 
@@ -35,3 +34,39 @@ md.tree <- phylo.maker(sp.list = md.df, scenarios="S1")
 md.tree <- md.tree$scenario.1
 
 save(md.tree, file = "clean.data/meadows-phylo.rda")
+
+#____________________________________________________________________
+
+#angiosperm only
+
+dat2 <- dat[, -(42:43)]
+
+
+md.sp.list <- colnames(dat2)
+md.g.list <- sapply(strsplit(md.sp.list, "_"), function(x) paste(x[1]))
+
+id2 <- which(md.sp.list %in% GBOTB.extended$tip.label)
+md.sp.list[-id2]
+md.sp.list[110] <- "Silene_flos-cuculi"
+
+md.f.list <- c()
+for (i in 1:length(md.g.list)) {
+  if (md.g.list[i] %in% gf$genus){
+    md.f.list <- c(md.f.list, gf$family[which(gf$genus %in% md.g.list[i])])
+  } else {
+    md.f.list <- c(md.f.list, NA)
+  }
+}
+
+md.f.list[103:105] <- "Polygonaceae"
+
+md.df <- data.frame(species = md.sp.list, genus = md.g.list, family = md.f.list)
+md.df[, c("species.relative", "genus.relative")] <- NA
+
+rm(id2, md.sp.list, md.g.list, md.f.list)
+
+md.tree2 <- phylo.maker(sp.list = md.df, scenarios="S1")
+md.tree2 <- md.tree2$scenario.1
+
+save(md.tree2, file = "clean.data/meadows-phylo-angiosp-only.rda")
+save(dat2, file = "clean.data/meadows-2018-angiosp-only.rda")
---
+++ 02.phylogenetic-analysis.r
@@ -0,0 +1,62 @@
+library(ape)
+library(picante)
+
+load("clean.data/meadows-phylo.rda")
+load("clean.data/meadows-2018.rda")
+
+#all species
+
+mat <- cophenetic(md.tree)
+colnames(mat)[53] <- "Silene_flos.cuculi"
+rownames(mat)[53] <- "Silene_flos.cuculi"
+
+ses.mpd <- ses.mpd(dat, mat, null.model = "independentswap", runs = 999, iterations = 1000)
+meta$mpd.z <- ses.mpd$mpd.obs.z
+meta$mpd.p <- ses.mpd$mpd.obs.p
+
+ses.mpd.a <- ses.mpd(dat, mat, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
+meta$mpd.a.z <- ses.mpd.a$mpd.obs.z
+meta$mpd.a.p <- ses.mpd.a$mpd.obs.p
+
+ses.mntd <- ses.mntd(dat, mat, null.model = "independentswap", runs = 999, iterations = 1000)
+meta$mntd.z <- ses.mntd$mntd.obs.z
+meta$mntd.p <- ses.mntd$mntd.obs.p
+
+ses.mntd.a <- ses.mntd(dat, mat, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
+
+meta$mntd.a.z <- ses.mntd.a$mntd.obs.z
+meta$mntd.a.p <- ses.mntd.a$mntd.obs.p
+
+save(meta, file = "clean.data/meadows-phylo-ses")
+
+#_________________________________________________________________________________________
+
+#angiosperm only
+
+load("clean.data/meadows-phylo-angiosp-only.rda")
+load("clean.data/meadows-2018-angiosp-only.rda")
+
+meta2 <- meta
+
+mat2 <- cophenetic(md.tree2)
+colnames(mat2)[53] <- "Silene_flos.cuculi"
+rownames(mat2)[53] <- "Silene_flos.cuculi"
+
+ses.mpd <- ses.mpd(dat2, mat2, null.model = "independentswap", runs = 999, iterations = 1000)
+meta2$mpd.z <- ses.mpd$mpd.obs.z
+meta2$mpd.p <- ses.mpd$mpd.obs.p
+
+ses.mpd.a <- ses.mpd(dat2, mat2, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
+meta2$mpd.a.z <- ses.mpd.a$mpd.obs.z
+meta2$mpd.a.p <- ses.mpd.a$mpd.obs.p
+
+ses.mntd <- ses.mntd(dat2, mat2, null.model = "independentswap", runs = 999, iterations = 1000)
+meta2$mntd.z <- ses.mntd$mntd.obs.z
+meta2$mntd.p <- ses.mntd$mntd.obs.p
+
+ses.mntd.a <- ses.mntd(dat2, mat2, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
+
+meta2$mntd.a.z <- ses.mntd.a$mntd.obs.z
+meta2$mntd.a.p <- ses.mntd.a$mntd.obs.p
+
+save(meta2, file = "clean.data/meadows-phylo-ses-ao")
