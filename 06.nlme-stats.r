4956ea4caa2b24a5ec59e94c18b5eba7103ca04c
basil-yakimov <damselfly@yandex.ru>
2019-11-11 15:33:19 +0300

    fix plot.effects()

--- 06.nlme-stats.r
+++ 06.nlme-stats.r
@@ -1,20 +1,6 @@
-load("clean.data/meadows-2018.rda")
-load("clean.data/traits-2018-61.rda")
-
-df <- data.frame(year = meta$year, 
-                 site = sapply(strsplit(as.character(meta$site), "-"), function(x) x[[1]]),
-                 id = paste0(sapply(strsplit(as.character(meta$site), "-"), function(x) x[[1]]), "-", c(rep(1:50, 2), rep(1:65, 15))))
-df$numsite <- as.numeric(df$site)
-
-
-dat <- dat[, row.names(tr)]
-pp <- dat / rowSums(dat)
-
-
-df$cwm.hgt <- as.matrix(pp) %*% tr$hgt
-df$cwm.sla <- as.matrix(pp) %*% tr$SLA
-df$cwm.ldmc <- as.matrix(pp) %*% tr$LDMC
-df$cwm.seed <- as.matrix(pp[, !is.na(tr$seed_mass)]) %*% tr$seed_mass[!is.na(tr$seed_mass)]
+#load("clean.data/meadows-2018.rda")
+#load("clean.data/traits-2018-61.rda")
+load("clean.data/meadows-df.rda")
 
 source("R/plot.effect.r")
 
@@ -25,19 +11,7 @@ plot.effect("cwm.ldmc")
 plot.effect("cwm.seed")
 par(op)
 
-
 #---#
-
-load("clean.data/meadows-single-trait-ses.rda")
-
-df$mpd.z.hgt <- meta.hgt$mpd.z
-df$mpd.a.z.hgt <- meta.hgt$mpd.a.z
-df$mpd.z.sla <- meta.SLA$mpd.z
-df$mpd.a.z.sla <- meta.SLA$mpd.a.z
-df$mpd.z.ldmc <- meta.LDMC$mpd.z
-df$mpd.a.z.ldmc <- meta.LDMC$mpd.a.z
-df$mpd.z.seed <- meta.seedmass$mpd.z
-df$mpd.a.z.seed <- meta.seedmass$mpd.a.z
 
 op <- par(mfcol = c(2, 2), mar = c(0.5, 4, 0.5, 0.5))
 plot.effect("mpd.z.hgt")
@@ -56,47 +30,12 @@ par(op)
 
 #---#
 
-library(FD)
-
-vec <- tr$hgt
-names(vec) <- row.names(tr)
-res <- dbFD(vec, pp, corr = "cailliez")
-
-df$fric.hgt <- res$FRic
-df$feve.hgt <- res$FEve
-df$mpd.a.hgt <- res$RaoQ
-
-vec <- tr$SLA
-names(vec) <- row.names(tr)
-res <- dbFD(vec, pp, corr = "cailliez")
-
-df$fric.sla <- res$FRic
-df$feve.sla <- res$FEve
-df$mpd.a.sla <- res$RaoQ
-
-vec <- tr$LDMC
-names(vec) <- row.names(tr)
-res <- dbFD(vec, pp, corr = "cailliez")
-
-df$fric.ldmc <- res$FRic
-df$feve.ldmc <- res$FEve
-df$mpd.a.ldmc <- res$RaoQ
-
-vec <- tr$seed_mass
-names(vec) <- row.names(tr)
-res <- dbFD(vec, pp, corr = "cailliez")
-
-df$fric.seed <- res$FRic
-df$feve.seed <- res$FEve
-df$mpd.a.seed <- res$RaoQ
-
 op <- par(mfcol = c(2, 2), mar = c(0.5, 4, 0.5, 0.5))
 plot.effect("fric.hgt")
 plot.effect("fric.sla")
 plot.effect("fric.ldmc")
 plot.effect("fric.seed")
 par(op)
-
 
 op <- par(mfcol = c(2, 2), mar = c(0.5, 4, 0.5, 0.5))
 plot.effect("feve.hgt")
@@ -113,28 +52,6 @@ plot.effect("mpd.a.seed")
 par(op)
 
 #---#
-
-res <- dbFD(tr[, 1:5], pp, corr = "cailliez")
-
-df$fric.5 <- res$FRic
-df$feve.5 <- res$FEve
-df$fdiv.5 <- res$FDiv
-df$mpd.a.5 <- res$RaoQ
-
-tr$age_first_flowering <- factor(tr$age_first_flowering)
-tr$branching <- factor(tr$branching)
-tr$leaf_distribution <- factor(tr$leaf_distribution)
-tr$growth_form <- factor(tr$growth_form)
-tr$dispersal <- factor(tr$dispersal)
-tr$life_span <- factor(tr$life_span)
-library(cluster)
-dist <- daisy(tr[, 6:11], metric = "gower")
-res <- dbFD(x = dist, a = pp, corr = "cailliez")
-
-df$fric.6 <- res$FRic
-df$feve.6 <- res$FEve
-df$fdiv.6 <- res$FDiv
-df$mpd.a.6 <- res$RaoQ
 
 op <- par(mfcol = c(2, 2), mar = c(0.5, 4, 0.5, 0.5))
 plot.effect("fric.5")
--- R/plot.effect.r
+++ R/plot.effect.r
@@ -1,10 +1,72 @@
-plot.effect <- function(var)
+# 
+# 
+# plot.effect.2 <- function(frame, ylab = "cwm.sla")
+# {
+#   require(nlme)
+#   require(effects)
+#   require(multcomp)
+#   
+#   #df <- data.frame(var = var, year = year, site = site, id = id)
+#   
+#   #f <- as.formula(paste0(var, " ~ site + year"))
+#   assign(".frame", frame, env = .GlobalEnv)
+#   fit <- lme(fixed = var ~ site + year, random = ~ 1 | id, .frame, method = "REML")
+#   
+#   an <- anova(fit)
+#   ef <- effect("site", fit)
+#   let <- cld(summary(glht(fit, linfct = mcp(site = "Tukey"))))$mcletters$Letters
+#   
+#   bp <- barplot(as.vector(ef$fit), col = c("tomato", "steelblue", "skyblue", "limegreen"), 
+#                 ylim = c(min(ef$lower), max(ef$upper) + (max(ef$upper) - min(ef$lower)) * 0.2 ),
+#                 ylab = ylab)
+#   arrows(x0 = bp, y0 = ef$lower,
+#          y1 = ef$upper, code = 3, 	angle = 90)
+#   text(x = bp, y = ef$upper,
+#        labels = as.vector(let),
+#        pos = 3)
+#   
+#   legend("top", legend = "", bty = "n", title = paste0("ANOVA p-values: site - ", round(an[2, 4], 3),
+#                                                        ", year - ", round(an[3, 4], 3)))
+# }
+# 
+# library(effects)
+# fc <- function(dta, formula, terms) 
+# {
+#   print(m1 <- lm(formula, .dta))
+#   Effect(terms, m1)
+# }
+# form <- prestige ~ income*type + education
+# terms <- c("income", "type")
+# fc(Duncan, form, terms)
+# 
+# fc.working <- function(dta, var, terms)
+# {
+#   assign(".dta", dta, env=.GlobalEnv)
+#   
+#   f <- as.formula(paste0(var, " ~ site + year"))
+#   assign(".f", f, env=.GlobalEnv)
+#   print(m1 <- lme(fixed = .f, random = ~ 1 | id, data = .frame, method = "REML"))
+#   Effect(terms, m1)
+#   plot(allEffects(m1))
+#   remove(".dta", envir=.GlobalEnv)
+# }
+# fc.working(Duncan, form, terms)
+# 
+# 
+# fc.working(df, "cwm.sla", "site")
+
+
+
+plot.effect <- function(var, frame = df)
 {
   require(nlme)
   require(effects)
+  require(multcomp)
   
-  f <- as.formula(paste0(var, "~ site + year"))
-  fit <- lme(f, random = ~ 1 | id, df[!is.na(df[, var]),], method = "REML")
+  assign(".frame", frame, env = .GlobalEnv)
+  f <- as.formula(paste0(var, " ~ site + year"))
+  assign(".form", f, env=.GlobalEnv)
+  fit <- lme(fixed = .form, random = ~ 1 | id, data = .frame, method = "REML", na.action = "na.omit")
   
   an <- anova(fit)
   ef <- effect("site", fit)
@@ -21,4 +83,68 @@ plot.effect <- function(var)
   
   legend("top", legend = "", bty = "n", title = paste0("ANOVA p-values: site - ", round(an[2, 4], 3),
                                                        ", year - ", round(an[3, 4], 3)))
+  #remove(.frame, env=.GlobalEnv)
 }
+
+# plot.effect(frame = df, "pc1")
+# 
+# 
+# plot.int <- function(var = "cwm.sla")
+# {
+#   require(nlme)
+#   require(effects)
+# 
+#   f <- as.formula(paste0(var, " ~ site * year"))
+#   fit <- lme(fixed = f, random = ~ 1 | id, data = df, method = "REML")
+#   plot(allEffects(fit))
+#   }
+
+# df <- data.frame(y = rnorm(90), x = gl(3, 30), b = factor(rep(1:30, 3)))
+# 
+# fit <- lme(fixed = y ~ x, random = ~ 1 | b, data = df, method = "REML")
+# ef <- effect("x", fit)
+# bp <- barplot(as.vector(ef$fit), col = c("tomato", "skyblue", "limegreen"),
+#         ylim = c(min(ef$lower), max(ef$upper) + (max(ef$upper) - min(ef$lower)) * 0.2 ))
+# arrows(x0 = bp, y0 = ef$lower, y1 = ef$upper, code = 3, angle = 90)
+# 
+# test1 <- function(y, x, b)
+# {
+#   
+#   assign(".y", y, env = .GlobalEnv)
+#   assign(".x", x, env = .GlobalEnv)
+#   fit <- lme(fixed = .y ~ .x, random = ~ 1 | b, method = "REML")
+#   print(summary(fit))
+#   ef <- effect(".x", fit)
+#   bp <- barplot(as.vector(ef$fit), col = c("tomato", "skyblue", "limegreen"),
+#                 ylim = c(min(ef$lower), max(ef$upper) + (max(ef$upper) - min(ef$lower)) * 0.2 ))
+#   arrows(x0 = bp, y0 = ef$lower, y1 = ef$upper, code = 3, angle = 90)
+#   #remove(".y", env = .GlobalEnv)
+#   #remove(".x", env = .GlobalEnv)
+# }
+# 
+# test1(df$y, df$x, df$b)
+# 
+# test2 <- function(y, x, b)
+# {
+#   frame <- data.frame(y, x, b)
+# 
+#   fit <- lme(fixed = y ~ x, random = ~ 1 | b, frame, method = "REML")
+#   ef <- effect("x", fit)
+#   bp <- barplot(as.vector(ef$fit), col = c("tomato", "skyblue", "limegreen"),
+#                 ylim = c(min(ef$lower), max(ef$upper) + (max(ef$upper) - min(ef$lower)) * 0.2 ))
+#   arrows(x0 = bp, y0 = ef$lower, y1 = ef$upper, code = 3, angle = 90)
+# }
+# 
+# test2(df$y, df$x, df$b)
+# 
+# 
+# test3 <- function(df) {
+#   fit <- lme(fixed = y ~ x, random = ~ 1 | b, data = df, method = "REML")
+#   ef <- effect("x", fit)
+#   bp <- barplot(as.vector(ef$fit), col = c("tomato", "skyblue", "limegreen"),
+#                 ylim = c(min(ef$lower),
+#                          max(ef$upper) + (max(ef$upper) - min(ef$lower)) * 0.2 ))
+#   arrows(x0 = bp, y0 = ef$lower, y1 = ef$upper, code = 3, angle = 90)
+# }
+# 
+# test3(data.frame(df$y, df$x, df$b))
